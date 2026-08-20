24003ce72a54bb15e5d6805dd98e15c53543c208
pkaygh <53969211+pkaygh@users.noreply.github.com>
2026-08-20 01:44:23 +0000

    Update API_URL in config.js
    Updated the API_URL in the CONFIG object with a new URL.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js - Store your Apps Script deployment URL here
 const CONFIG = {
   // REPLACE THIS WITH YOUR ACTUAL APPS SCRIPT URL
-  API_URL: 'https://script.google.com/macros/s/AKfycbx09dX-wkA8MvMI6tVZpQ6X4AcykVvfOqX0YZV36hSOz3FZ5S6se4X6QTGWABlHSK1j/exec'
+  API_URL: 'https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbx09dX-wkA8MvMI6tVZpQ6X4AcykVvfOqX0YZV36hSOz3FZ5S6se4X6QTGWABlHSK1j/exec'
 };
