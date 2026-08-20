407e99c0f9eb612d08f1da16705344b52d2dd363
pkaygh <53969211+pkaygh@users.noreply.github.com>
2026-08-20 02:24:30 +0000

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js - Store your Apps Script deployment URL here
 const CONFIG = {
   // REPLACE THIS WITH YOUR ACTUAL APPS SCRIPT URL
-  API_URL: 'https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbx09dX-wkA8MvMI6tVZpQ6X4AcykVvfOqX0YZV36hSOz3FZ5S6se4X6QTGWABlHSK1j/exec'
+  API_URL: 'https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbz5AaC859plp1avICGiyRpSiwXuVPTR0CCAKHD5plKc8aDQmT7Ll-ZtkQApiI8HUTOC/exec'
 };
